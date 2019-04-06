fb97c0a268d6602eb9f12fdc5937112dd43756d3
Tim Schoen <schoentr@gmail.com>
2019-04-05 17:16:12 -0700

    graphs testing complete

--- code-challanges/401_code_challenges/sorts/radix_sort/radix_sort.py
+++ code-challanges/401_code_challenges/sorts/radix_sort/radix_sort.py
@@ -1,70 +1,66 @@
-def radix_sort(inpt_list, base=10):
-    """This sort takes in  a list of positive  numbers and returns the list sorted in place.
-    
-    Arguments:
-        inpt_list {[list]} -- [Unsorted List]
-    
-    Keyword Arguments:
-        base {int} -- [description] (default: {10})
-    
-    Returns:
-        [list] -- [Sorted List]
-    """
-
-    if inpt_list == []:
+def radix_sort(alist, base=10):
+    if alist == []:
         return
  
     def key_factory(digit, base):
-        """This fuction creates the keys.  
-        
-        Arguments:
-            digit {[type]} -- [description]
-            base {[type]} -- [description]
-        
-        Returns:
-            [type] -- [description]
-        """
-
         def key(alist, index):
-            """This helper function takes in the unsorted list and and index and returns the digit at that location.
-            
-            Arguments:
-                alist {list} -- [unsorted list]
-                index {int} -- [index of list to be evaluated]
-            
-            Returns:
-                [int] -- [the digit that is in the place being evaluated]
-            """
-
-            return ((inpt_list[index]//(base**digit)) % base)
+            return ((alist[index]//(base**digit)) % base)
         return key
-    largest = max(inpt_list)
+    largest = max(alist)
     exp = 0
-    while base**exp <= largest: #checks to see if all values have been covered
-        inpt_list = count_sort(inpt_list, key_factory(exp, base),base-1)
+    while base**exp <= largest:
+        alist = counting_sort(alist, base - 1, key_factory(exp, base))
         exp = exp + 1
     return alist
-
-def count_sort(inpt_list,key,list_range):
-    """ This sorts a list by counting the number of occurances a given digt occurs, 
-    Arguments:
-        inpt_list {list} 
-        key {int}
-        list_range {int} 
-    
-    Returns:
-        [list] 
-    """
-
-    count_list= [0]*(list_range + 1)
-    for i in range(len(inpt_list)):
-        count_list[key(inpt_list, i)] = count_list[key(inpt_list, i)] + 1
-    count_list[0] = count_list[0] - 1 
-    for i in range(1, list_range + 1):
-        count_list[i] = count_list[i] + count_list[i - 1]
-    result = [None]*len(inpt_list)
-    for i in range(len(inpt_list) - 1, -1, -1):
-        result[count_list[key(inpt_list, i)]] = inpt_list[i]
-        count_list[key(inpt_list, i)] = count_list[key(inpt_list, i)] - 1
  
-    return result
+
+def counting_sort(arr, exp1): 
+  
+    n = len(arr) 
+  
+    # The output array elements that will have sorted arr 
+    output = [0] * (n) 
+  
+    # initialize count array as 0 
+    count = [0] * (10) 
+  
+    # Store count of occurrences in count[] 
+    for i in range(0, n): 
+        index = (arr[i]/exp1) 
+        count[ (index)%10 ] += 1
+  
+    # Change count[i] so that count[i] now contains actual 
+    #  position of this digit in output array 
+    for i in range(1,10): 
+        count[i] += count[i-1] 
+  
+    # Build the output array 
+    i = n-1
+    while i>=0: 
+        index = (arr[i]/exp1) 
+        output[ count[ (index)%10 ] - 1] = arr[i] 
+        count[ (index)%10 ] -= 1
+        i -= 1
+  
+    # Copying the output array to arr[], 
+    # so that arr now contains sorted numbers 
+    i = 0
+    for i in range(0,len(arr)): 
+        arr[i] = output[i]
+# def counting_sort(alist, largest, key):
+#     c = [0]*(largest + 1)
+#     for i in range(len(alist)):
+#         c[key(alist, i)] = c[key(alist, i)] + 1
+ 
+#     # Find the last index for each element
+#     c[0] = c[0] - 1 # to decrement each element for zero-based indexing
+#     for i in range(1, largest + 1):
+#         c[i] = c[i] + c[i - 1]
+ 
+#     result = [None]*len(alist)
+#     for i in range(len(alist) - 1, -1, -1):
+#         result[c[key(alist, i)]] = alist[i]
+#         c[key(alist, i)] = c[key(alist, i)] - 1
+ 
+#     return result
+    
---
+++ code-challanges/401_code_challenges/sorts/radix_sort/test_radix_sort.py
@@ -0,0 +1,26 @@
+from sorts.radix_sort.radix_sort import radix_sort
+import numpy as np
+
+def test_one():
+    arr=[1,2,3,56,223,78,24,90,13]
+    expected = [1,2,3,13,24,56,78,90,223]
+    radix_sort(arr)
+    assert expected == arr
+
+# def test_two():
+#     arr=['car','bar','tar','zar']
+#     expected = ['bar','car','tar','zar']
+#     quick_sort(arr)
+#     assert expected == arr
+
+def test_random():
+    arr = [i for i in np.random.randint(1,101,100)]
+    radix_sort(arr)
+    for j in range (1,len(arr)):
+        assert arr[j-1]<= arr[j]
+
+def test_empty_list():
+    arr= []
+    expected = []
+    radix_sort(arr)
+    assert expected == arr
